94d91de0a27f06994197a1e7ac6abea8f5f22972
Joanna <joanna.gargas@gmail.com>
2017-12-04 13:49:12 +0100

    Change pessimistic create and delete post to optimistic

--- src/app/posts-component/posts.component.ts
+++ src/app/posts-component/posts.component.ts
@@ -11,35 +11,36 @@ import { BadInput } from "../common/bad-input";
 })
 
 
-export class PostsComponent implements OnInit {
-
-  posts: any[];
-
+  export class PostsComponent implements OnInit {
 
   constructor(private service: PostService) {
-
-  }
-
-  ngOnInit(): void {
-    this.service.getAll()
-      .subscribe(posts => this.posts = posts);
   }
 
   createPost(input: HTMLInputElement) {
     let post = {title: input.value};
+    this.posts.splice(0, 0, post);
+
     input.value = '';
 
     this.service.create(post)
       .subscribe(newPost => {
-          post['id'] = newPost.id;
-          this.posts.splice(0, 0, post);
+        post['id'] = newPost.id;
         },
         (error: AppError) => {
+          this.posts.splice(0,1);
+
           if (error instanceof BadInput) {
-              // form.setErrors(error.errorValue);
+            // form.setErrors(error.errorValue);
           }
           else throw error;
-        });
+      });
+  }
+
+  posts: any[];
+
+  ngOnInit(): void {
+    this.service.getAll()
+      .subscribe(posts => this.posts = posts);
   }
 
   updatePost(post) {
@@ -51,13 +52,15 @@ export class PostsComponent implements OnInit {
   }
 
   deletePost(post) {
+    let index = this.posts.indexOf(post);
+    this.posts.splice(index, 1);
+
     this.service.delete(post.id)
       .subscribe(
-        () => {
-          let index = this.posts.indexOf(post);
-          this.posts.splice(index, 1)
-        },
+        null,
         (error: AppError) => {
+          this.posts.splice(index,0,post);
+
           if (error instanceof NotFoundError)
             alert('This post has already been deleted.');
           else throw error;
