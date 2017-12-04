63c269e83e213b1d5075df8693350f903904cac2
Joanna <joanna.gargas@gmail.com>
2017-12-04 23:27:30 +0100

    Create additional components (nav, home, not found, github profile)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,21 @@ import {PostService} from "./services/post.service";
 import {AppErrorHandler} from "./common/app-error-handler";
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
 import {GithubFollowersService} from "./services/github-followers.service";
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
-    GithubFollowersComponent
+    GithubFollowersComponent,
+    NavbarComponent,
+    HomeComponent,
+    GithubProfileComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
