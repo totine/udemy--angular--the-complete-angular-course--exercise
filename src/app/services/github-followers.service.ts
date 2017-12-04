b61ce1cd255b028347e8c6d33b2cf1f0b5fa2a49
Joanna <joanna.gargas@gmail.com>
2017-12-04 15:33:22 +0100

    Fill constructor for GithubFollowersService

--- src/app/services/github-followers.service.ts
+++ src/app/services/github-followers.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import {Http} from "@angular/http";
 
 @Injectable()
 export class GithubFollowersService {
 
-  constructor() { }
+  constructor(http: Http) {
+    super('https://api.github.com/users/mosh-hamedani/followers', http)
+  }
 
 }
