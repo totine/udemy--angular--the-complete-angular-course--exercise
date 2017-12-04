034d6e48f37d0c1b3644ea253545d1f5e6fffbd2
Joanna <joanna.gargas@gmail.com>
2017-12-04 23:29:55 +0100

    Import router module in app.module.ts and fix spaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {NgModule, ErrorHandler} from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts-component/posts.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {PostService} from "./services/post.service";
-import {AppErrorHandler} from "./common/app-error-handler";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { PostService } from "./services/post.service";
+import { AppErrorHandler } from "./common/app-error-handler";
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
-import {GithubFollowersService} from "./services/github-followers.service";
+import { GithubFollowersService } from "./services/github-followers.service";
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { GithubProfileComponent } from './github-profile/github-profile.component';
