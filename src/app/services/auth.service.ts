22ccabe075a5f338203f2ba077227bc90c170121
Joanna <joanna.gargas@gmail.com>
2017-12-07 12:23:03 +0100

    Implement login, logout and isLoggedIn in auth.service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map'
+import {JwtHelper} from "angular2-jwt";
 
 @Injectable()
 export class AuthService {
   constructor(private http: Http) {
   }
 
-  login(credentials) { 
-   return this.http.post('/api/authenticate', 
-      JSON.stringify(credentials));
+  login(credentials) {
+   return this.http.post('/api/authenticate',
+      JSON.stringify(credentials))
+     .map(response => {
+       let result = response.json();
+       if (result && result.token) {
+         localStorage.setItem('token', result.token);
+         return true;
+       }
+      return false;
+     });
   }
 
-  logout() { 
+  logout() {
+    localStorage.removeItem('token');
   }
 
-  isLoggedIn() { 
-    return false;
+  isLoggedIn() {
+    let jwtHelper = new JwtHelper();
+    let token = localStorage.getItem('token');
+
+    if (!token) {
+      return false;
+    }
+
+    let expirationDate = jwtHelper.getTokenExpirationDate(token);
+    let isExpired = jwtHelper.isTokenExpired(token);
+
+    return !isExpired;
   }
 }
 
